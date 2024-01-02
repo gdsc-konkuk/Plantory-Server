d13444c4e30bf22b02087980fdf012ea651ecdfc
jiwoo <zbqmgldjfh@gmail.com>
2024-01-02 21:59:44 +0900

    feat(BaseTimeEntity): 공통적으로 상속할 시간 클래스 작성

---
+++ src/main/kotlin/gdsc/plantory/common/config/JpaAuditingConfiguration.kt
@@ -0,0 +1,9 @@
+package gdsc.plantory.common.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+
+@Configuration
+@EnableJpaAuditing
+class JpaAuditingConfiguration {
+}
