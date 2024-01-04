8c7c7356bd74615e3e40a361568756927a13a7c3
jiwoo <zbqmgldjfh@gmail.com>
2024-01-04 12:28:03 +0900

    feat(Histories): 히스토리 일급컬렉션 작성 완료

---
+++ src/main/kotlin/gdsc/plantory/plant/domain/Histories.kt
@@ -0,0 +1,12 @@
+package gdsc.plantory.plant.domain
+
+import jakarta.persistence.CascadeType
+import jakarta.persistence.Embeddable
+import jakarta.persistence.OneToMany
+
+@Embeddable
+class Histories {
+
+    @OneToMany(mappedBy = "companion_plant", cascade = [CascadeType.PERSIST, CascadeType.MERGE], orphanRemoval = true)
+    private val histories: MutableList<History> = mutableListOf()
+}
