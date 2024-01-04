fbfaa7c1827bdeed5095d242472be523af7ec2a5
jiwoo <zbqmgldjfh@gmail.com>
2024-01-04 12:26:45 +0900

    feat(History): 히스토리 도메인 구현 완료

---
+++ src/main/kotlin/gdsc/plantory/plant/domain/History.kt
@@ -0,0 +1,33 @@
+package gdsc.plantory.plant.domain
+
+import gdsc.plantory.common.domain.BaseTimeEntity
+import jakarta.persistence.CascadeType
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.EnumType
+import jakarta.persistence.Enumerated
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
+import jakarta.persistence.Table
+
+@Entity
+@Table(name = "history")
+class History(
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "type", nullable = false)
+    private val type: HistoryType,
+
+    @ManyToOne(cascade = [CascadeType.PERSIST])
+    @JoinColumn(name = "companion_plant_id")
+    private val companionPlant: CompanionPlant? = null,
+
+    @Id
+    @Column(name = "history_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private val id: Long = 0L,
+) : BaseTimeEntity() {
+}
