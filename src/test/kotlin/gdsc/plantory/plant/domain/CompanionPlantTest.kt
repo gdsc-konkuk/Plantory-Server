c703d9661ebb9e830a9351ffff21311a0e33814f
jiwoo <zbqmgldjfh@gmail.com>
2024-01-04 14:52:57 +0900

    test(CompanionPlant): 반려식물 도메인 테스트 작성

---
+++ src/test/kotlin/gdsc/plantory/plant/domain/CompanionPlantTest.kt
@@ -0,0 +1,92 @@
+package gdsc.plantory.plant.domain
+
+import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatThrownBy
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+import java.time.LocalDate
+
+@DisplayName("도메인 : CompanionPlant")
+class CompanionPlantTest {
+
+    @Test
+    fun `반려식물 생성 테스`() {
+        Assertions.assertThatCode {
+            CompanionPlant(
+                "https://nongsaro.go.kr/cms_contents/301/14687_MF_ATTACH_01.jpg",
+                "shine", "나의 아기 선인장", LocalDate.now(), LocalDate.now().plusDays(7), 7
+            )
+        }
+            .doesNotThrowAnyException()
+    }
+
+    @Test
+    fun `기록 작성 테스트`() {
+        // given
+        val companionPlant = CompanionPlant(
+            "https://nongsaro.go.kr/cms_contents/301/14687_MF_ATTACH_01.jpg",
+            "shine", "나의 아기 선인장", LocalDate.now(), LocalDate.now().plusDays(7), 7
+        )
+
+        // when
+        companionPlant.saveRecord("test_comment", "https://nongsaro.go.kr/cms_contents/301/14687_MF_ATTACH_01.jpg")
+        companionPlant.saveRecord("test_comment")
+
+        // then
+        assertThat(companionPlant.recordSize()).isEqualTo(2)
+    }
+
+    @Test
+    fun `히스토리 생성 테스트`() {
+        // given
+        val companionPlant = CompanionPlant(
+            "https://nongsaro.go.kr/cms_contents/301/14687_MF_ATTACH_01.jpg",
+            "shine", "나의 아기 선인장", LocalDate.now(), LocalDate.now().plusDays(7), 7
+        )
+
+        // when
+        companionPlant.saveHistory(HistoryType.WATER_CHANGE, LocalDate.now())
+        companionPlant.saveHistory(HistoryType.POT_CHANGE, LocalDate.now())
+
+        // then
+        assertThat(companionPlant.historySize()).isEqualTo(2)
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = ["2023,1,1,1", "2023,1,7,7", "2023,2,1,32"])
+    fun `반려식물과 함께한 일수 계산`(year: Int, month: Int, day: Int, daySince: Int) {
+        // given
+        val companionPlant = CompanionPlant(
+            "https://nongsaro.go.kr/cms_contents/301/14687_MF_ATTACH_01.jpg",
+            "shine", "나의 아기 선인장", LocalDate.now(), LocalDate.now().plusDays(7),
+            7, LocalDate.of(2023, 1, 1)
+        )
+
+        // when
+        val result: Int = companionPlant.calculateDaySince(LocalDate.of(year, month, day))
+
+        // then
+        assertThat(result).isEqualTo(daySince)
+    }
+
+    @Test
+    fun `함께한날 계산시 생일 이전의 날을 입력하면 예외가 발생`() {
+        // given
+        val companionPlant = CompanionPlant(
+            "https://nongsaro.go.kr/cms_contents/301/14687_MF_ATTACH_01.jpg",
+            "shine", "나의 아기 선인장", LocalDate.now(), LocalDate.now().plusDays(7),
+            7, LocalDate.of(2023, 1, 1)
+        )
+
+        // when, then
+        assertThatThrownBy {
+            companionPlant.calculateDaySince(LocalDate.of(1995, 5, 30))
+        }
+            .isInstanceOf(IllegalArgumentException::class.java)
+            .hasMessageContaining("함께한 날은 음수가 될 수 없습니다.")
+    }
+}
