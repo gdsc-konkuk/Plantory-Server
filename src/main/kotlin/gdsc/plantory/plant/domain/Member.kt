d0cd275c1da62135ecd098741e100623351d400f
WhitePiano <startoflateness@gmail.com>
2024-01-06 22:24:00 +0900

    refactor: `device_code`에 대한 제약 제거

--- src/main/kotlin/gdsc/plantory/plant/domain/Member.kt
+++ src/main/kotlin/gdsc/plantory/plant/domain/Member.kt
@@ -8,13 +8,11 @@ import jakarta.persistence.Table
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 
-private const val DEVICE_TOKEN_LENGTH_LIMIT = 300
-
 @Entity
 @Table(name = "MEMBER")
 class Member(
 
-    @Column(name = "device_token", length = DEVICE_TOKEN_LENGTH_LIMIT, nullable = false)
+    @Column(name = "device_token", nullable = false)
     private val deviceToken: String,
 
     @Id
@@ -22,16 +20,6 @@ class Member(
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private val id: Long = 0L,
 ) : BaseTimeEntity() {
-
-    init {
-        if (deviceToken.length > DEVICE_TOKEN_LENGTH_LIMIT) {
-            throw IllegalArgumentException("${DEVICE_TOKEN_LENGTH_LIMIT}자를 초과하는 \"device_token\"은 존재하지 않습니다.")
-        }
-
-        if (deviceToken.isBlank()) {
-            throw IllegalArgumentException("\"device_token\"은 공백일 수 없습니다.")
-        }
-    }
 
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
--- src/test/kotlin/gdsc/plantory/plant/domain/MemberTest.kt
+++ src/test/kotlin/gdsc/plantory/plant/domain/MemberTest.kt
@@ -3,7 +3,6 @@ package gdsc.plantory.plant.domain
 import org.assertj.core.api.Assertions.assertThatCode
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertThrows
 
 @DisplayName("도메인 : Member")
 class MemberTest {
@@ -11,21 +10,5 @@ class MemberTest {
     @Test
     fun `멤버 생성 테스트`() {
         assertThatCode { Member("device_token") }.doesNotThrowAnyException()
-    }
-
-    @Test
-    fun `"device_token"이 공백이라면 예외를 반환`() {
-        assertThrows<IllegalArgumentException> { Member("") }
-    }
-
-    @Test
-    fun `"device_token" 길이 제한 초과하면 예외를 반환`() {
-        // length: 301
-        val longDeviceToken = "htfdsfdswwwgggggle.casdfdsrchq353EC%;;u;8%EB%8dsfajaslkfjasdkfjkldsafjlsadkjf" +
-                "lksajfldkadsfajaslkfjasdkfjkldsafjlsadkjflksajfldkajlafkj;lkdjalkfjads;jfalksdfjlasjf;" +
-                "ljlfsaddsfajaslkfjasdkfjkldsafjlsadkjflksajfldkajlafkj;lkdjalkfjads;jfalksdfjlasjf;ljlf" +
-                "sadjlafkj;lkdjalkfjads;jfalksdfjlasjf;ljdsfajaslkfj"
-
-        assertThrows<IllegalArgumentException> { Member(longDeviceToken) }
     }
 }
