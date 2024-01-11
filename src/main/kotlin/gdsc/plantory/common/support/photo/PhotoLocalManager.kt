0b7a1f5b8ec227882db7afd73c09832c04e068ef
jiwoo <zbqmgldjfh@gmail.com>
2024-01-11 16:57:26 +0900

    feat(PhotoLocalManager): 로컬에 사진 업로드 기능 구현 완료

---
+++ src/main/kotlin/gdsc/plantory/common/support/photo/PhotoLocalManager.kt
@@ -0,0 +1,66 @@
+package gdsc.plantory.common.support.photo
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Component
+import org.springframework.web.multipart.MultipartFile
+import java.io.File
+import java.io.IOException
+
+@Component
+class PhotoLocalManager(
+    @Value("\${local.image.root}")
+    private val localPath: String,
+) {
+
+    companion object {
+        private const val SLASH = "/"
+    }
+
+    fun upload(multipartFile: MultipartFile, workingDirectory: String): String {
+        require(!(multipartFile.isEmpty)) { "이미지 파일이 존재하지 않습니다" }
+        return uploadPhoto(multipartFile, workingDirectory)
+    }
+
+    private fun uploadPhoto(multipartFile: MultipartFile, workingDirectory: String): String {
+        return try {
+            val fileName: String = PhotoNameGenerator.of(multipartFile.originalFilename)
+            val uploadDirectory = loadDirectory(getLocalDirectoryPath(workingDirectory))
+            val uploadPath = File(uploadDirectory, fileName)
+
+            uploadFileInLocal(multipartFile, uploadPath)
+
+            StringBuilder()
+                .append(SLASH)
+                .append(workingDirectory)
+                .append(SLASH)
+                .append(fileName)
+                .toString()
+        } catch (e: Exception) {
+            throw IllegalStateException("파일 업로드 실패")
+        }
+    }
+
+    private fun getLocalDirectoryPath(workingDirectory: String): String {
+        return StringBuilder()
+            .append(localPath)
+            .append(SLASH)
+            .append(workingDirectory)
+            .toString()
+    }
+
+    private fun loadDirectory(directoryLocation: String): File {
+        val directory = File(directoryLocation)
+        if (!directory.exists()) {
+            directory.mkdirs()
+        }
+        return directory
+    }
+
+    private fun uploadFileInLocal(multipartFile: MultipartFile, uploadPath: File) {
+        try {
+            multipartFile.transferTo(uploadPath)
+        } catch (e: IOException) {
+            throw IllegalStateException("파일 변환이 실패했습니다.")
+        }
+    }
+}
