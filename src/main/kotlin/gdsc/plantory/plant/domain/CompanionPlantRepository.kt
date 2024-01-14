3f2344e64d321f67316538dc4db228fd35303dfc
WhitePiano <startoflateness@gmail.com>
2024-01-14 13:58:09 +0900

    refactor: 불필요한 직접 쿼리 제거

--- src/main/kotlin/gdsc/plantory/plant/domain/CompanionPlantRepository.kt
+++ src/main/kotlin/gdsc/plantory/plant/domain/CompanionPlantRepository.kt
@@ -1,9 +1,7 @@
 package gdsc.plantory.plant.domain
 
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.jpa.repository.Query
 
 interface CompanionPlantRepository : JpaRepository<CompanionPlant, Long> {
-    @Query(value = "SELECT plant FROM CompanionPlant plant WHERE plant.id = :memberId")
-    fun findAllByMemberid(memberId: Long): MutableList<CompanionPlant>
+    fun findAllByMemberId(memberId: Long): List<CompanionPlant>
 }
