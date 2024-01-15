16878ccaa9c939f97e5cd53d3bb87132406790be
jiwoo <zbqmgldjfh@gmail.com>
2024-01-15 16:51:13 +0900

    feat(HistoryType): String을 받아 HistoryType Enum으로 변환하는 메서드 작성

--- src/main/kotlin/gdsc/plantory/plant/domain/HistoryType.kt
+++ src/main/kotlin/gdsc/plantory/plant/domain/HistoryType.kt
@@ -1,5 +1,11 @@
 package gdsc.plantory.plant.domain
 
 enum class HistoryType {
-    WATER_CHANGE, POT_CHANGE, RECORDING
+    WATER_CHANGE, POT_CHANGE, RECORDING;
+
+    companion object {
+        fun byNameIgnoreCaseOrNull(input: String): HistoryType? {
+            return entries.firstOrNull { it.name.equals(input, true) }
+        }
+    }
 }
---
+++ src/test/kotlin/gdsc/plantory/plant/domain/HistoryTypeTest.kt
@@ -0,0 +1,36 @@
+package gdsc.plantory.plant.domain
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import java.util.stream.Stream
+
+class HistoryTypeTest {
+
+    @ParameterizedTest
+    @MethodSource("historyTypeInputProvider")
+    fun `문자열로부터 Enum으로 변환`(input: String, historyType: HistoryType) {
+        // when
+        val result = HistoryType.byNameIgnoreCaseOrNull(input)
+
+        // then
+        assertThat(result).isEqualTo(historyType)
+    }
+
+
+    companion object {
+        @JvmStatic
+        private fun historyTypeInputProvider(): Stream<Arguments?>? {
+            return Stream.of<Arguments?>(
+                Arguments.of("WATER_CHANGE", HistoryType.WATER_CHANGE),
+                Arguments.of("water_change", HistoryType.WATER_CHANGE),
+                Arguments.of("POT_CHANGE", HistoryType.POT_CHANGE),
+                Arguments.of("pot_change", HistoryType.POT_CHANGE),
+                Arguments.of("RECORDING", HistoryType.RECORDING),
+                Arguments.of("recording", HistoryType.RECORDING)
+            )
+        }
+    }
+
+}
