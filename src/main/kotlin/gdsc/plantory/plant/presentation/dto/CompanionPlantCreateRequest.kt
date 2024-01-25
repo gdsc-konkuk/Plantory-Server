5930771c008497b21d4fb4c6807f396e96966d20
WhitePiano <startoflateness@gmail.com>
2024-01-25 15:49:55 +0900

    fix: 반려식물 생성 시, `PlantInformation`이 제대로 설정되지 않던 문제 해결

--- src/main/kotlin/gdsc/plantory/plant/presentation/dto/CompanionPlantCreateRequest.kt
+++ src/main/kotlin/gdsc/plantory/plant/presentation/dto/CompanionPlantCreateRequest.kt
@@ -17,9 +17,9 @@ data class CompanionPlantCreateRequest(
     @PastOrPresent(message = "마지막 물주기 날짜는 과거 또는 현재의 날짜여야 합니다. lastWaterDate: \${validatedValue}")
     @DateTimeFormat(pattern = "yyyy-MM-dd") val lastWaterDate: LocalDate,
 ) {
-    fun toEntity(imagePath: String, memberId: Long, waterCycle: Int): CompanionPlant {
+    fun toEntity(imagePath: String, memberId: Long, waterCycle: Int, plantInformationId: Long): CompanionPlant {
         // TODO : Cloud 환경으로 이전 후 제거, 로컬 사진 저장 테스트 용도
-        val baseUrl: String = "https://nongsaro.go.kr/"
+        val baseUrl = "https://nongsaro.go.kr/"
         return CompanionPlant(
             _imageUrl = baseUrl + imagePath,
             _shortDescription = this.shortDescription,
@@ -29,6 +29,7 @@ data class CompanionPlantCreateRequest(
             waterCycle = waterCycle,
             birthDate = this.birthDate,
             memberId = memberId,
+            plantInformationId = plantInformationId
         )
     }
 }
