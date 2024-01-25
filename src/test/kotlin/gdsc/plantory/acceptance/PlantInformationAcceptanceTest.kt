5031600e634101ece42c81fb87be44b336fc85a4
WhitePiano <startoflateness@gmail.com>
2024-01-25 20:54:05 +0900

    refactor: 식물 정보 인수테스트에 display name 추가 및 AcceptanceTest 상속

--- src/test/kotlin/gdsc/plantory/acceptance/PlantInformationAcceptanceTest.kt
+++ src/test/kotlin/gdsc/plantory/acceptance/PlantInformationAcceptanceTest.kt
@@ -2,9 +2,12 @@ package gdsc.plantory.acceptance
 
 import gdsc.plantory.acceptance.PlantInformationStep.Companion.식물_정보_조회_요청
 import gdsc.plantory.acceptance.PlantInformationStep.Companion.식물_정보_조회_응답_확인
+import gdsc.plantory.util.AcceptanceTest
+import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 
-class PlantInformationAcceptanceTest {
+@DisplayName("인수 : PlantInformation")
+class PlantInformationAcceptanceTest : AcceptanceTest() {
 
     @Test
     fun `식물 정보 조회`() {
