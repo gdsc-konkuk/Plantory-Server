93891bb9cfdf90f4ee094479e1ce9d85a70c7a84
jiwoo <zbqmgldjfh@gmail.com>
2024-02-04 17:28:58 +0900

    feat(PlantoryApplication): 서울로 시간 변경

--- src/main/kotlin/gdsc/plantory/PlantoryApplication.kt
+++ src/main/kotlin/gdsc/plantory/PlantoryApplication.kt
@@ -2,10 +2,17 @@ package gdsc.plantory
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import java.util.TimeZone
+import javax.annotation.PostConstruct
 
 @SpringBootApplication
 class PlantoryApplication
 
 fun main(args: Array<String>) {
     runApplication<PlantoryApplication>(*args)
+
+    @PostConstruct
+    fun setTimezone() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"))
+    }
 }
